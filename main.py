0b7fdbe2093c8a3cd7cc488dc679f289b62db83e
Panyloi <kocdecke@gmail.com>
2024-04-11 08:49:23 +0200

    checkpoint 1.3

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from random import random
 
 from concave_hull import concave_hull, concave_hull_indexes
 
+epsilon = 10e-12
+
 def get_cmap(n, name='hsv'):
     cols = (plt.cm.get_cmap(name)
             (np.linspace(0, 1.0, n))[:, :3][:,::-1])
@@ -29,14 +31,6 @@ def interpolate_points(points, num=100):
 
 def convert_points(points, flag_radious, num_of_points = 20):
     points_copy = deepcopy(points)
-
-    # only for points
-    # for point in points_copy:
-    #     points.append((point[0], point[1] + flag_radious))
-    #     points.append((point[0], point[1] - flag_radious))
-    #     points.append((point[0] - flag_radious, point[1]))
-    #     points.append((point[0] + flag_radious, point[1]))
-
     # circle
     angles = np.linspace(0, 2*np.pi, num_of_points, endpoint=False)
 
@@ -47,6 +41,91 @@ def convert_points(points, flag_radious, num_of_points = 20):
             points.append((x, y))
 
     return np.array(points)
+
+def distance(a, b):
+    return np.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
+
+def orient(p,q,r):
+        result = ((p[0] - r[0])*(q[1] - r[1]) - (p[1] - r[1])*(q[0] - r[0]))
+        if result > epsilon:
+            return 1
+        elif result < -epsilon:
+            return -1
+        else:
+            return 0
+
+# similarity to Jarvis algorithms
+"""
+params:
+    points : set of circle points 
+             [[(x11, y11), (x21, y21), ..., (xn1, yn1)], [(x12, y12), (x22, y22), ..., (xm2, ym2)], ...]
+
+return:
+    points that create outline for cluster points
+
+"""
+def greedy_selecting(points):
+    # handle 1 point cluster
+    if len(points) <= 1: return points
+
+    num_of_sets = len(points)
+    first_points_set = set(points)
+
+    p0 = min(points[0], key=lambda x: (x[1], x[0]))
+    
+    # actual points to return
+    outline_points = []
+
+    # lists of considered points
+    current_points_array = points[0]
+    next_points_array = points[1]
+
+    # point that is currently handle
+    current_point = p0
+
+    # second_point = next_points_array[0]
+    # last point considered
+    last_point = current_points_array[0] if current_points_array[0] != p0 else current_points_array[1]
+
+
+    while(True):
+
+        # create new set of points
+        points_union = current_points_array + next_points_array
+        
+        for i in range(len(points_union)):
+            # if points_union[i] in next_points_array and orient(current_point, second_point, points_union[i]) == 1:
+            # #    (orient(current_point, second_point, points_union[i]) == 0 and distance(second_point, current_point) < distance(current_point, current_point[i])):
+            #     second_point = points_union[i]
+            if orient(current_point, last_point, points_union[i] == 1) or \
+               (orient(current_point, last_point, points_union[i]) == 0 and distance(last_point, current_point) < distance(current_point, points_union[i])):
+                last_point = points_union[i]
+
+    
+
+
+def jarvisAlgorithmUpgrade(points_pass):
+    
+    p0 = min(points_pass, key=lambda x: (x[1],x[0]))
+
+    points_of_hull = [p0] # points that inculde in convex hull
+    lastpoint = points_pass[0] if points_pass[0] != p0 else points_pass[1] # the latest point to check if it can be in convex hull
+    curr_point = p0 # current point of convex hull
+
+    while (True):
+
+        for i in range(len(points_pass)):
+            if (orient(curr_point, lastpoint, points_pass[i]) == -1 ) or (orient(curr_point, lastpoint, points_pass[i]) == 0 and distance(lastpoint, curr_point) < distance(curr_point, points_pass[i])):
+                lastpoint = points_pass[i]
+        
+        curr_point = lastpoint
+
+        if curr_point == p0:
+            break
+            
+        points_of_hull.append(curr_point)
+    
+
 
 def visualizate_points(categorized_names, gruped_points, ax, with_iterpolation = True):
     # get random cmap for drawning hulls
@@ -87,7 +166,7 @@ def visualizate_points(categorized_names, gruped_points, ax, with_iterpolation =
         # END_NEW
 
 
-        new_points = interpolate_points(_idx_points, 10)
+        new_points = interpolate_points(_idx_points, 100)
 
         if with_iterpolation:
             line_to_plot = [
