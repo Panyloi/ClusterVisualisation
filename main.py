43b776e2a18fb36609cc93f945ec317c80bb33fb
Panyloi <kocdecke@gmail.com>
2024-04-11 21:48:41 +0200

    Pierre points draw covex hull

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from matplotlib.collections import LineCollection
 from scipy import interpolate
 from copy import deepcopy
 from random import random
+import time
+from sklearn import preprocessing as pr
 
 from concave_hull import concave_hull, concave_hull_indexes
 
@@ -31,16 +33,19 @@ def interpolate_points(points, num=100):
 
 def convert_points(points, flag_radious, num_of_points = 20):
     points_copy = deepcopy(points)
+    jarvis_points = []
     # circle
     angles = np.linspace(0, 2*np.pi, num_of_points, endpoint=False)
 
     for point in points_copy:
+        new_set_of_points = []
         for angle in angles:   
             x = point[0] + flag_radious * np.cos(angle)
             y = point[1] + flag_radious * np.sin(angle)
             points.append((x, y))
-
-    return np.array(points)
+            new_set_of_points.append((x, y))
+        jarvis_points.append(new_set_of_points)
+    return np.array(points), jarvis_points
 
 def distance(a, b):
     return np.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
@@ -66,15 +71,20 @@ return:
 """
 def greedy_selecting(points):
     # handle 1 point cluster
-    if len(points) <= 1: return points
+    if len(points) <= 1: return points[0]
+    print("WITAM")
+    print(points)
+    time.sleep(2)
 
     num_of_sets = len(points)
-    first_points_set = set(points)
+    first_points_set = set(points[0])
 
     p0 = min(points[0], key=lambda x: (x[1], x[0]))
     
     # actual points to return
     outline_points = []
+
+    i_array = 1
 
     # lists of considered points
     current_points_array = points[0]
@@ -82,27 +92,40 @@ def greedy_selecting(points):
 
     # point that is currently handle
     current_point = p0
-
-    # second_point = next_points_array[0]
+    second_point = next_points_array[0]
     # last point considered
     last_point = current_points_array[0] if current_points_array[0] != p0 else current_points_array[1]
 
+    # print("WITAM")
 
     while(True):
 
         # create new set of points
         points_union = current_points_array + next_points_array
-        
+
         for i in range(len(points_union)):
             # if points_union[i] in next_points_array and orient(current_point, second_point, points_union[i]) == 1:
             # #    (orient(current_point, second_point, points_union[i]) == 0 and distance(second_point, current_point) < distance(current_point, current_point[i])):
             #     second_point = points_union[i]
-            if orient(current_point, last_point, points_union[i] == 1) or \
+        
+            if orient(current_point, last_point, points_union[i]) == 1 or \
                (orient(current_point, last_point, points_union[i]) == 0 and distance(last_point, current_point) < distance(current_point, points_union[i])):
+                if points_union[i] in next_points_array:
+                    second_point = points_union[i]
                 last_point = points_union[i]
 
-            
+        current_point = last_point
+        i_array += 1
+        
+        if second_point in first_points_set:
+            break
+        
+        current_points_array = next_points_array
+        next_points_array = points[i_array%len(points)]
 
+        outline_points.append(current_point)
+
+    return outline_points
 
 def jarvisAlgorithmUpgrade(points_pass):
     
@@ -126,11 +149,10 @@ def jarvisAlgorithmUpgrade(points_pass):
         points_of_hull.append(curr_point)
     
 
-
 def visualizate_points(categorized_names, gruped_points, ax, with_iterpolation = True):
     # get random cmap for drawning hulls
-    cmap = get_cmap(len(categorized_names))
-    np.random.shuffle(cmap)
+    # cmap = get_cmap(len(categorized_names))
+    # np.random.shuffle(cmap)
 
     for i in range(len(categorized_names)):
         # reshape points into correct shape
@@ -151,10 +173,29 @@ def visualizate_points(categorized_names, gruped_points, ax, with_iterpolation =
 
         _idx_points = [zipped_points[idxes[i]] for i in range(len(idxes))]
 
-        # NEW
         # zipped_points = convert_points(_idx_points, 20, num_of_points=10)
-        zipped_points = convert_points(_idx_points, 10**6)
+        # zipped_points, to_jarvis = convert_points(_idx_points, 10**6, num_of_points=10)
         # zipped_points = convert_points(_idx_points, 0.02)
+        zipped_points = convert_points(_idx_points, 0.02)[0]
+
+        # start reverse jarvis algorithm
+
+        # NEW
+
+        # outline_points = np.array(greedy_selecting(to_jarvis))
+        # # print("OUTLINE POINTS")
+        # # print(outline_points)
+
+        # idxes = concave_hull_indexes(
+        #     outline_points[:, :2],
+        #     concavity=2,
+        #     length_threshold=20,
+        # )
+
+        # _idx_points = [outline_points[idxes[i]] for i in range(len(idxes))]
+
+        # END_NEW
+
 
         idxes = concave_hull_indexes(
             zipped_points[:, :2],
@@ -163,8 +204,6 @@ def visualizate_points(categorized_names, gruped_points, ax, with_iterpolation =
         )
 
         _idx_points = [zipped_points[idxes[i]] for i in range(len(idxes))]
-        # END_NEW
-
 
         new_points = interpolate_points(_idx_points, 100)
 
@@ -177,16 +216,51 @@ def visualizate_points(categorized_names, gruped_points, ax, with_iterpolation =
             line_to_plot = [(zipped_points[idxes[j]], zipped_points[idxes[(j+1)%len(idxes)]])
                             for j in range(len(idxes))]
 
-        line_segments = LineCollection(segments = np.array(line_to_plot), colors = cmap[i])
+        # line_segments = LineCollection(segments = np.array(line_to_plot), colors = cmap[i])
+        line_segments = LineCollection(segments = np.array(line_to_plot))
         ax.add_collection(line_segments)
 
-def main():
+def get_pierre_syntetic(df):
+    df_scatter = df.copy()
+    df["instance_id"] = df["instance_id"].apply(lambda x: x.split(sep="_")[0])
 
-    # prepare given points
-    # df = pd.read_csv("./points/kk_swap_2d.csv", sep=';')
-    df = pd.read_csv("./points/kamada_l1-mutual_attraction_2d.csv", sep=';')
-    # df = pd.read_csv("./points/prefsynth_dataset/coordinates/mds_dapl2_2d.csv", sep=';')
+    x_points = df["x"].to_numpy()
+    y_points = df["y"].to_numpy()
+    rows = []
+    flag = False
 
+    for row in df.iterrows():
+        if row[1]["instance_id"] == "syntetic":
+            flag = not flag
+            continue
+
+        if flag:
+            rows.append(pd.Series([row[1]["instance_id"], row[1]["x"], row[1]["y"]]))
+    
+    new_df = pd.DataFrame([list(rows[i]) for i in range(len(rows))], columns = ["instance_id", "x", "y"])
+    categorized_names = new_df["instance_id"].unique()
+    gruped_points = {}
+
+    for i in range(new_df["instance_id"].nunique()):
+        selected_points = new_df.loc[new_df["instance_id"] == categorized_names[i]]
+        gruped_points[categorized_names[i]] = {"x": selected_points["x"].to_numpy(),
+                                               "y": selected_points["y"].to_numpy()}
+    
+    fig, ax = plt.subplots()
+    # ax.scatter(x_points, y_points, s=20)
+    # df.plot.scatter(x= "x", y= "y", c= "instance_id", colormap="hsv")
+
+    # df_scatter[['type', 'instance_id']] = df_scatter.instance_id.str.split(pat='_', expand=True)
+    df_scatter['type_no'] = pr.LabelEncoder().fit(df_scatter["instance_id"].unique()).transform(df_scatter["instance_id"])
+
+    ax.scatter(df_scatter.x, df_scatter.y, c=df_scatter.type_no, cmap="hsv")
+    ax.set_title('Simple plot')
+
+    visualizate_points(categorized_names, gruped_points, ax, with_iterpolation=False)
+    
+    plt.show()
+
+def get_normal_show(df):
     df["instance_id"] = df["instance_id"].apply(lambda x: x.split(sep="_")[0])
 
     x_points = df["x"].to_numpy()
@@ -199,7 +273,7 @@ def main():
     for i in range(df["instance_id"].nunique()):
         selected_points = df.loc[df["instance_id"] == categorized_names[i]]
         gruped_points[categorized_names[i]] = {"x": selected_points["x"].to_numpy(),
-                                               "y": selected_points["y"].to_numpy()}
+                                            "y": selected_points["y"].to_numpy()}
 
     fig, ax = plt.subplots()
     ax.scatter(x_points, y_points, s=20)
@@ -209,6 +283,22 @@ def main():
     
     plt.show()
 
+def main(pierre = False):
+
+    # prepare given points
+    # df = pd.read_csv("./points/kk_swap_2d.csv", sep=';')
+    # df = pd.read_csv("./points/kamada_l1-mutual_attraction_2d.csv", sep=';')
+    # df = pd.read_csv("./points/prefsynth_dataset/coordinates/mds_dapl2_2d.csv", sep=';')
+    df = pd.read_csv("./points/pierre.csv", sep=';')
+    
+    if pierre:
+        get_pierre_syntetic(df)
+    else:
+        get_normal_show(df)
+
+        
+
 
 if __name__ == "__main__":
-    main()
+    pierre = True
+    main(pierre)
